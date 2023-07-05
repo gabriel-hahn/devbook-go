2b95d0e4be357498fcd223a740347ae028eb7a8b
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2023-07-05 19:36:55 -0300

    feat: add response handler on controllers validations

---
+++ controllers/response.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.WriteHeader(statusCode)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Error(w http.ResponseWriter, statusCode int, err error) {
+	type Error struct {
+		Message string `json:"message"`
+	}
+
+	JSON(w, statusCode, Error{
+		Message: err.Error(),
+	})
+}
--- controllers/users.go
+++ controllers/users.go
@@ -2,9 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
-	"log"
 	"net/http"
 
 	"github.com/gabriel-hahn/devbook/database"
@@ -15,27 +13,31 @@ import (
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		Error(w, http.StatusUnprocessableEntity, err)
+		return
 	}
 
 	var user models.User
 	if err = json.Unmarshal(body, &user); err != nil {
-		log.Fatal(err)
+		Error(w, http.StatusBadRequest, err)
+		return
 	}
 
 	db, err := database.Connect()
 	if err != nil {
-		log.Fatal(err)
+		Error(w, http.StatusInternalServerError, err)
+		return
 	}
 	defer db.Close()
 
 	repository := repositories.NewUserRepository(db)
-	userID, err := repository.Create(user)
+	user.ID, err = repository.Create(user)
 	if err != nil {
-		log.Fatal(err)
+		Error(w, http.StatusInternalServerError, err)
+		return
 	}
 
-	w.Write([]byte(fmt.Sprintf("ID: %d", userID)))
+	JSON(w, http.StatusCreated, user)
 }
 
 func FindAllUsers(w http.ResponseWriter, r *http.Request) {

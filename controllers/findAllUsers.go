e48478e4b80c345bca2df2b10a8b14a5c6ba6d89
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2023-07-05 20:36:51 -0300

    feat: add list all users by user query (nick or name)

--- controllers/findAllUsers.go
+++ controllers/findAllUsers.go
@@ -1,7 +1,35 @@
 package controllers
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/gabriel-hahn/devbook/database"
+	"github.com/gabriel-hahn/devbook/repositories"
+)
 
 func FindAllUsers(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Find all users"))
+	nameOrNick := strings.ToLower(r.URL.Query().Get("user"))
+
+	if nameOrNick == "" {
+		Error(w, http.StatusBadRequest, errors.New("the query param 'user' is required"))
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.NewUserRepository(db)
+	users, err := repository.FindAllByFilters(nameOrNick)
+	if err != nil {
+		Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	JSON(w, http.StatusOK, users)
 }
--- repositories/users.go
+++ repositories/users.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/gabriel-hahn/devbook/models"
 )
@@ -32,4 +33,33 @@ func (u Users) Create(user models.User) (uint64, error) {
 	}
 
 	return uint64(ID), nil
+}
+
+func (u Users) FindAllByFilters(nameOrNick string) ([]models.User, error) {
+	nameOrNick = fmt.Sprintf("%%%s%%", nameOrNick)
+
+	rows, err := u.db.Query("select id, name, nick, email, created_at from users where name LIKE ? or nick LIKE ?", nameOrNick, nameOrNick)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []models.User
+	for rows.Next() {
+		var user models.User
+
+		if err = rows.Scan(
+			&user.ID,
+			&user.Name,
+			&user.Nick,
+			&user.Email,
+			&user.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
 }

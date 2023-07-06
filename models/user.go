1b4d3f58381ffaa81f048734ce94ef168e961162
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2023-07-06 10:52:05 -0300

    feat: add email format validation

--- models/user.go
+++ models/user.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"strings"
 	"time"
+
+	"github.com/badoux/checkmail"
 )
 
 type User struct {
@@ -48,6 +50,9 @@ func (u *User) validate(step ValidationType) error {
 	}
 	if step == Signup && u.Email == "" {
 		return errors.New("email is required")
+	}
+	if err := checkmail.ValidateFormat(u.Email); err != nil {
+		return errors.New("invalid email format")
 	}
 	if step == Signup && u.Password == "" {
 		return errors.New("password is required")
